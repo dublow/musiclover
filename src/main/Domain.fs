d9070e89e0d217bc228e5377cd1a931e973852f0
ndelfour <ndelfour@fastbooking.net>
2019-03-04 19:33:50 +0100

    start to write domain

---
+++ src/main/Domain.fs
@@ -0,0 +1,23 @@
+﻿namespace musiclover
+
+module providers = 
+    open FSharp.Data
+
+    type DjamProvider = JsonProvider<"Documents\Jsons\djam.json">
+
+module domain = 
+    open System
+
+    type Radio = {
+        Name:string
+        Uri:Uri
+    }
+
+    type Artist = {
+        Name:string
+    }
+
+    type Music = {
+        Title:string
+        Artist:Artist
+    }
