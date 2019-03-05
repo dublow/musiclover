52f6d3f31d7bf2f21f72ff9c6d4c55e50ba5c549
ndelfour <ndelfour@fastbooking.net>
2019-03-05 17:27:25 +0100

    wip

--- src/core/domain.fs
+++ src/core/domain.fs
@@ -1,51 +1,83 @@
-﻿namespace musiclover
+﻿namespace musiclover.core
 
-module providers = 
-    open FSharp.Data
+    module providers = 
+        open FSharp.Data
+        type DjamProvider = JsonProvider<"documents\jsons\djam.json">
 
-    type DjamProvider = JsonProvider<"documents\jsons\djam.json">
+    module domain = 
+        open System
 
-module domain = 
-    open System
+        type DeezerId = DeezerId of int
+        type SpotifyId = SpotifyId of string
 
-    type DeezerId = DeezerId of int
-    type SpotifyId = SpotifyId of string
+        type AllPlatform = {
+            DeezerId:DeezerId
+            SpotifyId:SpotifyId
+        }
 
-    type AllPlatform = {
-        DeezerId:DeezerId
-        SpotifyId:SpotifyId
-    }
+        type Platform = 
+        | Empty
+        | Deezer of DeezerId
+        | Spotify of SpotifyId
+        | All of AllPlatform
 
-    type Platform = 
-    | Empty
-    | Deezer of DeezerId
-    | Spotify of SpotifyId
-    | All of AllPlatform
+        type Radio = {
+            Name:string
+            Uri:Uri
+        }
 
-    type Radio = {
-        Name:string
-        Uri:Uri
-    }
+        type Artist = {
+            Name:string
+        }
 
-    type Artist = {
-        Name:string
-    }
+        type Track = {
+            Title:string
+            Duration: int
+            Artist:Artist
+            Platform:Platform
+        }
 
-    type Track = {
-        Title:string
-        Duration: int
-        Artist:Artist
-        Platform:Platform
-    }
+    module ``interface`` = 
+        open System.Threading.Tasks
+        open System
+        open System.Net
 
-module ``interface`` = 
-    open System.Net.Http
-    open System.Threading.Tasks
+        type ContentType = 
+        | ApplicationJson
 
-    type PostRequest = {
-        Uri:string
-        HttpContent:HttpContent
-    }
+        type MusicHttpResponse = {
+            Code:HttpStatusCode
+            Content:string
+        }
 
-    type IMusicHttpClient = 
-        abstract member PostAsync: PostRequest -> (HttpResponseMessage -> 'a) -> Task<'a>
+        type MusicHttpPostRequest = {
+            Uri:Uri
+            ContentType:ContentType
+            Body:string
+        }
+
+        type IMusicHttpClient = 
+            abstract member PostAsync: MusicHttpPostRequest -> (MusicHttpResponse -> 'a) -> Task<'a>
+            abstract member GetAsync: Uri -> (MusicHttpResponse -> 'a) -> Task<'a>
+
+    module dto =
+        open providers
+        open domain
+
+        let toTracks text = 
+            let toPlatform (deezer:option<DjamProvider.Deezer>) (spotify:option<DjamProvider.Spotify>) = 
+                match deezer, spotify with
+                | Some d, Some s -> All {DeezerId = DeezerId d.Id; SpotifyId = SpotifyId s.Id}
+                | Some d, None -> Deezer (DeezerId d.Id)
+                | None, Some s -> Spotify (SpotifyId s.Id)
+                | None, None -> Empty
+
+            let response = DjamProvider.Parse text
+                        
+            [for track in response.Tracks -> 
+                { Title = track.Title;
+                  Duration = track.Duration;
+                  Artist = {Name = track.Artist}
+                  Platform = (toPlatform track.Deezer track.Spotify)
+                  }
+            ]
---
+++ src/infrastructure/adapter.fs
@@ -0,0 +1,42 @@
+﻿namespace musiclover.adapter
+
+    module http = 
+        open System.Net.Http
+        open FSharp.Control.Tasks
+        open musiclover.core.``interface``
+        open System.Text
+
+        type MusicHttpClient =
+            member private this.ToMusicHttpResponseAsync(httpResponseMessage:HttpResponseMessage) = 
+                task {
+                    let! content = httpResponseMessage.Content.ReadAsStringAsync()
+                    return { Code = httpResponseMessage.StatusCode; Content = content }
+                }
+
+            member private this.ToHttpContent contentType body = 
+                let toMediaType contentType = 
+                    match contentType with
+                    | ApplicationJson -> "application/json"
+                
+                new StringContent(body, Encoding.UTF8, toMediaType contentType)
+
+            interface IMusicHttpClient with
+                member this.PostAsync request map = 
+                    task {
+                        use httpClient = new HttpClient()
+
+                        use body = this.ToHttpContent request.ContentType request.Body
+                        let! httpResponse = httpClient.PostAsync(request.Uri, body)
+                        let! response = this.ToMusicHttpResponseAsync(httpResponse)
+
+                        return map(response)
+                    }
+                member this.GetAsync uri map = 
+                    task{
+                        use httpClient = new HttpClient()
+
+                        let! httpResponse = httpClient.GetAsync(uri)
+                        let! response = this.ToMusicHttpResponseAsync(httpResponse)
+
+                        return map(response)
+                    }
